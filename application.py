2bf650dfd38702cd00fa983238a4da4ace6b3411
Victor255 <tagbar95@gmail.com>
2015-12-14 10:43:50 -0600

    first version of the project

--- application.py
+++ application.py
@@ -1,3 +1,138 @@
-# Aqui escribe tu codigo
+"SEARCH ENGINE"
 
-print "Hello World"
+import os
+import sys
+import urllib2
+import re
+
+class search_engine(object):
+	"""docstring for ClassName"""
+	def __init__(self):
+		pass
+
+	def menu(self):
+		while True:
+			self.clean_screen()
+			self.shows_menu()
+			two_menu = self.two_menu()
+			self.true_menu(two_menu)
+			self.menu_decision(two_menu)
+
+	def shows_menu(self):
+		print "1.SEARCH WORD"
+		print "2.EXIT"
+
+	def two_menu(self):
+		option_menu = raw_input("Type the number or option you want do: ")
+		option_menu = self.minuscule(option_menu)
+		return option_menu
+
+	
+	def minuscule(self, option_menu):
+		option_menu = option_menu.lower()
+		return option_menu
+
+	
+	def true_menu(self, option_menu):
+		if option_menu == "search word" or option_menu == "exit" or option_menu == "1" or option_menu == "2":
+			return "Valid option"
+		else:
+			return "Invalid option"
+
+	def menu_decision(self, two_menu):
+		if two_menu == "search word" or two_menu == "1":
+			self.search()
+		elif two_menu == "exit" or two_menu == "2":
+			self.exit()
+		else:
+			message = raw_input("Invalid option")
+
+	def search(self):
+		self.clean_screen()
+		valid_url = "Incorrect urls"
+		while valid_url == "Incorrect urls":
+			word, url1, url2 = self.ask_url()
+			page_html1 = self.url_one(url1)
+			page_html2 = self.url_two(url2)
+			valid_url = self.valid_url(page_html1, page_html2)
+		page_one = self.page_one(word, page_html1)
+		page_two = self.page_two(word, page_html2)
+		verifie_word = self.verifie_word(word, page_one, page_two, url1, url2)
+
+	def ask_url(self):
+		word = raw_input("Enter the word you want to search: ")
+		url1 = raw_input("Enter the first URL: ")
+		url2 = raw_input("Enter the second URL: ")
+		return word, url1, url2
+
+	def url_one(self, url1):
+		try:
+			page1 = urllib2.urlopen(url1)
+			page_html1 = page1.read()
+			return page_html1
+		except:
+			try:
+				url1 = "http://%s" % url1
+				page_html1 = self.url_one(url1)
+				return page_html1
+			except:
+				message = raw_input("The first URL is incorrect")
+				return "Incorrect url"
+
+	def url_two(self, url2):
+		try:
+			page2 = urllib2.urlopen(url2)
+			page_html2 = page2.read()
+			return page_html2
+		except:
+			try:
+				url2 = "http://%s" % url2
+				page_html2 = self.url_two(url2)
+				return page_html2
+			except:
+				message = raw_input("URL incorrect")
+				return "Incorrect url"
+
+	def valid_url(self, page_html1, page_html2):
+		if page_html1 == "Incorrect url" or page_html2 == "Incorrect url":
+			valid_url = "Incorrect urls"
+			message = raw_input("You need type again the information!!!")
+			self.clean_screen()
+			return valid_url
+		else:
+			return "Correct urls"
+
+	
+	def page_one(self, word, page_html1):
+		page_one = page_html1.count(word)
+		return page_one
+
+	
+	def page_two(self, word, page_html2):
+		page_two = page_html2.count(word)
+		return page_two
+
+	def verifie_word(self, word, page_one, page_two, url1, url2):
+		if page_one > page_two:
+			print "THIS IS THE URL THAT CONTAINS MORE THE WORD: ---> %s <--- APPEARS %s TIMES" % (url1, page_one)
+		elif page_two > page_one:
+			print "THIS IS THE URL THAT CONTAINS MORE THE WORD: ---> %s <--- APPEARS %s TIMES" % (url2, page_two)
+		elif page_one == page_two and page_one != 0 and page_two != 0:
+			print "The word is repeated the same quatity of times in both pages"
+		elif page_one == 0 and page_two == 0:
+			print "The word you are searching is not in any page"
+		message = raw_input("PRESS ENTER")
+
+	def clean_screen(self):
+		os.system('reset')
+
+	def exit(self):
+		self.clean_screen()
+		sys.exit()
+
+def application():
+	my_program = search_engine()
+	my_program.menu()
+
+if __name__ == '__main__':
+	application()
