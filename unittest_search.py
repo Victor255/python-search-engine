13adfba5a48c195f4b45ba8806b40a03a0732ac9
Victor255 <tagbar95@gmail.com>
2015-12-22 12:07:10 -0600

    With Unittest

--- unittest_search.py
+++ unittest_search.py
@@ -1,28 +1,28 @@
 import application
 import unittest
 
-class Test_Search_Engine(unittest.TestCase):
-	"""docstring for Test_Search_Engine"""
+class Test_SearchEngine(unittest.TestCase):
+	"""docstring for Test_SearchEngine"""
 
 	#Test true_menu
 	def test_true_menu(self):
-		class_application = application.search_engine()
+		class_application = application.SearchEngine()
 		self.assertEqual(class_application.true_menu("1"), "Valid option")
 		self.assertEqual(class_application.true_menu("EXIT"), "Invalid option")
 
 	#Test minuscule
 	def test_minuscule(self):
-		class_application = application.search_engine()
+		class_application = application.SearchEngine()
 		self.assertTrue(class_application.minuscule("WORLD").islower())
 
 	#Test page_one
 	def test_page_one(self):
-		class_application = application.search_engine()
+		class_application = application.SearchEngine()
 		self.assertTrue(type(class_application.page_one("Carro", "https://es.wikipedia.org/wiki/Carro")) == int)
 
 	#Test page_two
 	def test_page_two(self):
-		class_application = application.search_engine()
+		class_application = application.SearchEngine()
 		self.assertTrue(type(class_application.page_two("Carro", "http://www.olx.com.ve/carros-cat-378")) == int)
 
 if __name__ == '__main__':
